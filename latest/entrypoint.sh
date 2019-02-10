3afe8e4fa846679cf4b922b0271fe1352a785d76
Yannick Stein <y@nnick.me>
2019-02-10 22:49:03 +0100

    Alles speichern

--- latest/entrypoint.sh
+++ latest/entrypoint.sh
@@ -9,17 +9,28 @@ KNOWNHOSTFILE="./known_hosts";
 else
 KNOWNHOSTFILE="${HOME}/.ssh/known_hosts";
 fi
+ISINITCOMMAND=FALSE
 
 while [ "$1" != "" ]; do
   case "$1" in
-    --add-known-host ) echo "Get SSH-Key from $2"; 
-      ssh-keyscan $2 > $KNOWNHOSTFILE;
-      shift 2 ;;
+    --add-known-host )
+      ISINITCOMMAND=TRUE
+      shift ;
+      echo "Get SSH-Key from $1"; 
+      ssh-keyscan $1 > $KNOWNHOSTFILE;
+      shift ;;
+    --generate-ssh-key )
+      ISINITCOMMAND=TRUE;
+      echo "Generate SSH-Keys";
+      ssh-keygen -b 4096 -t rsa -f "${HOME}/.ssh/id_rsa" -q -N "";
+      cat "${HOME}/.ssh/id_rsa.pub";
+      shift;;
     * ) shift;;
   esac
 
 done
-
+if [ "$ISINITCOMMAND" == "FALSE" ]; then
 duplicity $OPTIONS
+fi
 
 exit 0
